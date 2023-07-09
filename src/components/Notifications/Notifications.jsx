47bfa4efa5b02d1fd59dd6ad498bbf84a71c5582
Rubel' Nata <119401527+7Nata7@users.noreply.github.com>
2023-07-09 17:20:55 +0300

    4

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 
 import css from './Notifications.module.css'
 
-export const Notification = ({ message }) => <p className={css.message}>{message}</p>;
+export const Notification = ({ message }) => 
+
+<p className={css.message}>{message}</p>;
 
 Notification.propTypes = {
  message: PropTypes.string.isRequired
